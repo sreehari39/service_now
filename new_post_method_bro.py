33749fd34e8722f3715093948fcf3b40db85b80b
sreehari <sreeh3979@gmail.com>
2023-11-02 17:31:34 +0530

    post method is added

--- new_post_method_bro.py
+++ new_post_method_bro.py
@@ -1,1 +1,18 @@
 
+from flask import Flask ,request
+from flask_restful import Resource, Api 
+
+app = Flask(__name__) # here we are creating Flask app
+api = Api(app) # here we creating instance of Flask
+
+class First(Resource):
+    def get(self):
+        return {"status": "success", "msg": "api created"}, 200
+    def post(self):
+         body = request.get_json()
+         return {"status":"success",'result' :body["a"] + body["b"]},201
+        
+api.add_resource(First, "/dev/v1/api/status")
+
+if __name__ == "__main__":
+    app.run(host="192.168.55.102", port=8055)
